1a7283fe4d42eb21bd736ba15cc5898f16798c39
Pett97 <henriquepeterson138@gmail.com>
2024-12-16 00:03:39 -0300

    initial

---
+++ resources/js/app.js
@@ -0,0 +1,4 @@
+import './bootstrap';
+import 'materialize-css/dist/js/materialize.min.js';
+import 'materialize-css/dist/css/materialize.min.css';
+
---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Api Jogos</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="nav-wrapper teal darken-1">   
+                <a href="#" class="brand-logo center">Api Jogos</a>
+            </div>
+        </nav>
+    </header>
+    @vite('resources/js/app.js')
+</body>
+
+</html>
